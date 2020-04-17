df9c83e0540c544bae77bfd961d5e7f0e553ed9a
neelratanguria <neelratan@live.com>
2020-04-17 18:57:17 +0530

    Sign up implemented

--- app/src/main/java/com/firebird/signinapp/Signin.java
+++ app/src/main/java/com/firebird/signinapp/Signin.java
@@ -1,46 +1,72 @@
 package com.firebird.signinapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Signin extends AppCompatActivity {
 
-    EditText username, password;
+    EditText email, password;
     Button signInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        username = findViewById(R.id.username);
+        email = findViewById(R.id.username);
         password = findViewById(R.id.password);
         signInButton = findViewById(R.id.signinBtn);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userUsername = "";
+                String userEmail = "";
                 String userPassword = "";
 
-                userUsername = username.getText().toString();
+                userEmail = email.getText().toString();
                 userPassword = password.getText().toString();
 
-                if (userUsername.equals("Neel") && userPassword.equals("Neel"))
-                {
-                    Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Wrong username or password",
-                            Toast.LENGTH_LONG).show();
-                }
+                signUp(userEmail, userPassword);
             }
         });
     }
+
+    public void signUp(String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Neel", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Neel", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
